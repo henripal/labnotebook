5d72d2529c11df2b2ea2e2da13bd9daac4f44a17
henripal <henri.palacci@gmail.com>
2018-03-18 13:01:28 -0400

    more fixing of setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.1',                                                                   
     packages=find_packages(),                                                        
     include_package_data=True,
+    py_modules=['labnotebook', 'labnotebook.backend'],
     install_requires=[
         'sqlalchemy',
         'flask',
